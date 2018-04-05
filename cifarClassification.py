31e1c92f0dd92d8ad4376a1ccd6e5b161f0ccc8c
Nick Monto <monto.nicholas@gmail.com>
2018-04-05 14:27:15 -0400

    Cleaned up the tensorboard graph.
    Histograms need work.

--- cifarClassification.py
+++ cifarClassification.py
@@ -1,4 +1,4 @@
-from tensorflow.contrib.keras.api.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.contrib.keras.api.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation
 import tensorflow as tf
 
 LOGDIR = "/tmp/IntelAIworkshop/"
@@ -34,6 +34,7 @@ class TB(tf.keras.callbacks.TensorBoard):
 training_scaled = x_train / x_train.max()
 test_scaled = x_test / x_test.max()
 
+
 # One hot encoding of labels
 labels_train = tf.keras.utils.to_categorical(y_train, 10)
 labels_test = tf.keras.utils.to_categorical(y_test, 10)
@@ -41,45 +42,52 @@ labels_test = tf.keras.utils.to_categorical(y_test, 10)
 # Define the model: 4 convolutional layers, 2 max pools
 model = tf.keras.Sequential()
 
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=training_scaled.shape[1:],
-                 strides=(1, 1),
-                 activation='relu',
-                 # kernel_initializer='TruncatedNormal',
-                 name='conv1'))
+with tf.variable_scope('conv1'):
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=training_scaled.shape[1:],
+                     strides=(1, 1),
+                     # kernel_initializer='TruncatedNormal',
+                     name='conv1'))
+    model.add(Activation('relu'))
 
-model.add(Conv2D(32, (3, 3), padding='same',
-                 strides=(1, 1),
-                 activation='relu',
-                 # kernel_initializer='TruncatedNormal',
-                 name='conv2'))
+with tf.variable_scope('conv2'):
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     strides=(1, 1),
+                     # kernel_initializer='TruncatedNormal',
+                     name='conv2'))
+    model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), name='pool1'))
 model.add(Dropout(0.25, name='dropout1'))
 
-model.add(Conv2D(64, (3, 3), padding='same',
-                 strides=(1, 1),
-                 activation='relu',
-                 # kernel_initializer='TruncatedNormal',
-                 name='conv3'))
+with tf.variable_scope('conv3'):
+    model.add(Conv2D(64, (3, 3), padding='same',
+                     strides=(1, 1),
+                     # kernel_initializer='TruncatedNormal',
+                     name='conv3'))
+    model.add(Activation('relu'))
 
-model.add(Conv2D(64, (3, 3), padding='same',
-                 strides=(1, 1),
-                 activation='relu',
-                 # kernel_initializer='TruncatedNormal',
-                 name='conv4'))
+with tf.variable_scope('conv4'):
+    model.add(Conv2D(64, (3, 3), padding='same',
+                     strides=(1, 1),
+                     # kernel_initializer='TruncatedNormal',
+                     name='conv4'))
+    model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), name='pool2'))
 
 model.add(Flatten(name='flat1'))  # converts feature maps to feature vectors
 
-model.add(Dense(256,
-                activation='relu',
-                # kernel_initializer='TruncatedNormal',
-                name='fc1'))
+with tf.variable_scope('fc1'):
+    model.add(Dense(1024,
+                    # kernel_initializer='TruncatedNormal',
+                    name='fc1'))
+    model.add(Activation('relu'))
+
 model.add(Dropout(0.5, name='dropout2'))  # reset half of the weights to zero
 
-model.add(Dense(10,
-                activation='softmax',
-                name='output'))
+with tf.variable_scope('output'):
+    model.add(Dense(10,
+                    name='output'))
+    model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
